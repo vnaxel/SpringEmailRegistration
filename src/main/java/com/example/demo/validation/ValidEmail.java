a7d2593c390af86de527d33b40cbb38dc2f0d74e
vnaxel <axel.yvenou@gmail.com>
2021-12-10 19:36:38 +0100

    custom email validator

---
+++ src/main/java/com/example/demo/validation/ValidEmail.java
@@ -0,0 +1,19 @@
+package com.example.demo.validation;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
+
+@Target({TYPE, FIELD, ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = EmailValidator.class)
+public @interface ValidEmail {
+    String message() default "Invalid email";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default{};
+
+}
