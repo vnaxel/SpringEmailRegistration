b7f6be3c238fb74132323ee21f0534b219b0f0a5
vnaxel <axel.yvenou@gmail.com>
2021-12-27 07:03:08 +0100

    email confirmation controller + templates

--- src/main/java/com/example/demo/web/controller/RegistrationController.java
+++ src/main/java/com/example/demo/web/controller/RegistrationController.java
@@ -1,10 +1,13 @@
 package com.example.demo.web.controller;
 
 import com.example.demo.persistence.model.User;
+import com.example.demo.persistence.model.VerificationToken;
+import com.example.demo.registration.OnRegistrationCompleteEvent;
 import com.example.demo.service.IUserService;
 import com.example.demo.web.dto.UserDto;
 import com.example.demo.web.error.UserAlreadyExistException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,15 +15,21 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
+import java.util.Calendar;
 
 @Controller
 public class RegistrationController {
 
     @Autowired
     private IUserService userService;
+
+    @Autowired
+    ApplicationEventPublisher eventPublisher;
 
     @GetMapping("/registration")
     public String showRegistrationForm(WebRequest request, Model model) {
@@ -31,20 +40,49 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String registerUserAccount(@ModelAttribute("user") @Valid UserDto userDto,
-                                      BindingResult result, Model model) {
-
+                                      BindingResult result, HttpServletRequest request) {
 
         try {
             if (result.hasErrors()) {
                 return "registration";
             }
-            final User registered = userService.registerNewUserAccount(userDto);
-            return "successRegister";
+            User registered = userService.registerNewUserAccount(userDto);
+            String appUrl = request.getContextPath();
+            eventPublisher.publishEvent(new OnRegistrationCompleteEvent(registered, appUrl));
         } catch (final UserAlreadyExistException ueaEx) {
             String err = ueaEx.getMessage();
             result.addError(new ObjectError("globalError", err));
             return "registration";
+
+        } catch (RuntimeException ex) {
+            String err = ex.getMessage();
+            result.addError(new ObjectError("globalError", err));
+            return "registration";
         }
+        return "successRegister";
+    }
+
+    @GetMapping("/registrationConfirm")
+    public String confirmRegistration(WebRequest request, Model model, @RequestParam("token") String token) {
+
+        VerificationToken verificationToken = userService.getVerificationToken(token);
+        if (verificationToken == null) {
+            String err = "Invalid token";
+            model.addAttribute("err", err);
+            return "badUser";
+        }
+
+        Calendar cal = Calendar.getInstance();
+        if ((verificationToken.getExpiryDate().getTime() - cal.getTime().getTime()) <= 0) {
+            String err = "Your registration token has expired. Please register again.";
+            model.addAttribute("err", err);
+            return "badUser";
+        }
+
+        User user = verificationToken.getUser();
+        user.setEnabled(true);
+        userService.saveRegisteredUser(user);
+        return "registrationConfirm";
     }
 }
-            // TODO: email validation
+
